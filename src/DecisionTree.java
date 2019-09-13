e8560c582aaee1b159826832da1fed1e7e82fbad
mmthant03 <mmthant@wpi.edu>
2019-09-13 15:18:04 -0400

    nth to commit

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,5 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class DecisionTree {
+
+    String[] headers;
+
+
+
     public static void main(String[]args) {
         //TODO
+        String inputFile = args[0];
+        String outputFile = args[1];
+
+
+
+    }
+
+    public void importCsv(String inputFile) throws FileNotFoundException {
+        BufferedReader input = new BufferedReader(new FileReader(inputFile));
+        String row = "";
+        while ((row = csvReader.readLine()) != null) {
+            String[] data = row.split(",");
+            // do something with the data
+        }
+        csvReader.close();
     }
 }
---
+++ src/FeatureExtraction/Feature.java
@@ -0,0 +1,32 @@
+package FeatureExtraction;
+
+public class Feature {
+    int rows = 6;
+    int columns = 7;
+    public int[][] board = new int[rows][columns];
+    private int winner;
+    private int bottomLeft;
+    private int center;
+    private int bottomRight;
+
+    public Feature(int[][] board, int winner) {
+        this.setBoard(board);
+        this.winner = winner;
+        // after setting up the board, will use that board to set up the feature
+        // will go like this
+        // this.bottomLeft = BottomLeft();
+        // this.center = Center();
+
+    }
+
+    public void setBoard(int[][] newBoard) {
+        for(int i=0; i<this.rows; i++) {
+            for (int j=0; j < this.columns; j++) {
+                this.board[i][j] = newBoard[i][j];
+            }
+        }
+    }
+
+
+
+}
