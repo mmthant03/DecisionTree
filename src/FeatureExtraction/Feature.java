5842bce85aeb8956cdec4c69e90d502f7c8c0248
mmthant03 <mmthant@wpi.edu>
2019-09-14 13:57:03 -0400

    Added a feature called higherChances

--- src/FeatureExtraction/Feature.java
+++ src/FeatureExtraction/Feature.java
@@ -8,6 +8,7 @@ public class Feature {
     private int bottomLeft;
     private int center;
     private int bottomRight;
+    private int higherChances;
 
     /**
      * Constructor
@@ -21,6 +22,7 @@ public class Feature {
         // will go like this
         // this.bottomLeft = BottomLeft();
         // this.center = Center();
+        this.setHigherChances();
 
     }
 
@@ -28,13 +30,14 @@ public class Feature {
      * Display the board
      */
     public void display() {
+        System.out.println("\n=====================================\n");
         for (int i=rows-1; i>=0; i--) {
             for (int j = 0; j < columns; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println();
+        System.out.println("\nWinner piece : " + this.winner + "\n");
     }
 
     /**
@@ -49,6 +52,34 @@ public class Feature {
         }
     }
 
+    public int getHigherChances() {
+        return this.higherChances;
+    }
 
+    public void setHigherChances() {
+        // set the evaluation table
+        int[][] evaluationTable =   {{3, 4, 5, 7, 5, 4, 3},
+                                    {4, 6, 8, 10, 8, 6, 4},
+                                    {5, 8, 11, 13, 11, 8, 5},
+                                    {5, 8, 11, 13, 11, 8, 5},
+                                    {4, 6, 8, 10, 8, 6, 4},
+                                    {3, 4, 5, 7, 5, 4, 3}};
+        // set an array that corresponds to pieces i.e 0,1, and 2
+        int[] winChances = new int[3];
+        // adds up the chances according to evaluation table
+        for(int i=0;i<this.rows;i++) {
+            for (int j=0;j<this.columns;j++) {
+                winChances[this.board[i][j]] += evaluationTable[i][j];
+            }
+        }
+        // compare the chances and return the piece which show higher chances
+        if(winChances[0] >= winChances[1] && winChances[0]>= winChances[2]) {
+            this.higherChances = 0;
+        } else if (winChances[1] > winChances[2]) {
+            this.higherChances = 1;
+        } else if (winChances[2] > winChances[1]) {
+            this.higherChances = 2;
+        }
+    }
 
 }
